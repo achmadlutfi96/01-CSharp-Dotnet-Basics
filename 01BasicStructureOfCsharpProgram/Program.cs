4da70196c64e103e99760e960ff7308fade562be
achmad lutfi <46985830+achmadlutfi96@users.noreply.github.com>
2024-10-15 23:22:50 +0700

    Add project files.

---
+++ 01BasicStructureOfCsharpProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01BasicStructureOfCsharpProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
